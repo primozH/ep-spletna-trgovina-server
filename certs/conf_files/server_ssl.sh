f44487c5055d4bef9df9f5e3b0fc3f6c911f9204
Primož Hrovat <primoz.hrovat.96@gmail.com>
2018-01-12 13:21:32 +0100

    conf files

--- certs/conf_files/server_ssl.sh
+++ certs/conf_files/server_ssl.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 # INSTALL REQUIREMENTS
-sudo apt get php...
-
+sudo apt-get install -y python-software-properties
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt-get update -y
+sudo apt-get install -y git php7.1 libapache2-mod-php7.1 php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-mysql php7.1-mcrypt php7.1-zip
 
 # INSTALL LARAVEL
-composer update
+cd
+mkdir composer
+cd composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
 
-php artisan storage:link
-php artisan migrate:fresh --seed
 
+cd /var/www/html
+sudo git clone https://github.com/primozH/ep-spletna-trgovina-server spletna-trgovina
+cd spletna-trgovina
+
+cp certs/conf_files/.env .
 
 # DOVOLJENJA
-sudo chowm -R www-data /var/www/html/spletna-trgovina
-sudo chmod -R 775 /var/www/html/spletna-trgovina
+sudo chown -R www-data /var/www/html/spletna-trgovina
+sudo chmod -R 777 /var/www/html/spletna-trgovina
+
+composer install
 
 # APACHE CONF
 # SSL certifikati
@@ -25,6 +36,8 @@ sudo cp /var/www/html/spletna-trgovina/certs/server/*.pem .
 cd /etc/apache2/sites-available
 sudo cp /var/www/html/spletna-trgovina/certs/conf_files/*.conf .
 
+sudo a2enmod ssl
+sudo a2enmod rewrite
 sudo a2ensite spletna-trgovina.conf
 sudo a2ensite spletna-trgovina-ssl.conf
 
